3caf447ba811216e6fe0f1d7314ae27eb783956e
veexit <veexit@gmail.com>
2019-02-04 13:14:40 +0200

    add form to first screen

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Screens',
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var _nameFieldController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Firs Page'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new TextField(
+              controller: _nameFieldController,
+              decoration: new InputDecoration(
+                labelText: 'Enter text:'
+              ),
+            )
+          ),
+          new ListTile(
+            title: new RaisedButton(
+                child: new Text('Send to next screen'),
+                onPressed: null
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
